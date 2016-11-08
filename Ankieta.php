0d00a7a6e8e1724126459eff1a3b1e8cd82a5b03
Michal Bacciarelli <michal.bacciarelli@gmail.com>
2016-11-08 15:27:27 +0100

    Stworzenie pliku Ankieta.php

---
+++ Ankieta.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * create table Users(
+                        id int AUTO_INCREMENT NOT NULL,
+                        name varchar(255) NOT NULL,
+                        email varchar(255) NOT NULL,
+                        password varchar(60) NOT NULL,
+                        info text,
+                        PRIMARY KEY(id))
+     ENGINE=InnoDB, CHARACTER SET=utf8"
+ * 
+ */
+
+class Ankieta {
+    private $name;
+    private $link;
+    
+    
+    
+}
+
+
+
+?>
